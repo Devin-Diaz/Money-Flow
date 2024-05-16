6179d644b0117b561ef563580fd16470e4e8a709
Devin-Diaz <diazdevin7@outlook.com>
2024-05-16 10:48:39 -0400

    new budget, and adding expense logic is working

--- src/main/java/com/diazdevin/financetracker/Controller/ExpenseController.java
+++ src/main/java/com/diazdevin/financetracker/Controller/ExpenseController.java
@@ -1,8 +1,10 @@
 package com.diazdevin.financetracker.Controller;
 
 import com.diazdevin.financetracker.Dto.ExpenseDto;
+import com.diazdevin.financetracker.Dto.UserDto;
 import com.diazdevin.financetracker.Security.CustomUserDetails;
 import com.diazdevin.financetracker.Service.ExpenseService;
+import com.diazdevin.financetracker.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,12 +15,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigDecimal;
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/expenses")
 public class ExpenseController {
 
     @Autowired
     private ExpenseService expenseService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/new-expense")
     public String newExpense(Model model) {
@@ -45,10 +53,27 @@ public class ExpenseController {
 
     private Long getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails) {
-            CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        if (authentication != null && authentication.getPrincipal() instanceof CustomUserDetails userDetails) {
             return userDetails.getId();
         }
         throw new RuntimeException("User not authenticated");
     }
+
+    @GetMapping("/new-budget")
+    public String getBudgetPage() {
+        return "new-budget";
+    }
+
+    @PostMapping("/new-budget")
+    public String setBudget(@ModelAttribute("budget") BigDecimal budget, Principal principal) {
+        // Assuming you have a way to get the user ID from the principal
+        // This might vary based on your authentication implementation
+        Long userId = getCurrentUserId();
+        userService.initializeBudget(userId, budget);
+        return "redirect:/user-page"; // Redirect to user dashboard or appropriate page
+    }
+
+
+
+
 }
